6d0619acd842cfb36e6f3b577c0e91fce16073ed
ChenGeoP <62390830+ChenGeoP@users.noreply.github.com>
2020-04-21 05:55:14 +0800

    Add files via upload

---
+++ Untitled9.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Read me
+# This notebook will will be mainly used for *the capstone project*
+
+# In[1]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import os
+import time as tm
+from datetime import datetime as dt
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
